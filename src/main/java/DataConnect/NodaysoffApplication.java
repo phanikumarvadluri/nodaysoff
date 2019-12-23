b9fb504a834ce2c541bfdc041ed96a9ab816bcad
Phani <phani.vadluri@gmail.com>
2019-12-23 22:44:42 +0530

    Initial commit

---
+++ src/main/java/DataConnect/NodaysoffApplication.java
@@ -0,0 +1,13 @@
+package DataConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodaysoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodaysoffApplication.class, args);
+	}
+
+}
